e084d60c8340eded8ec87725c47862ad5fcf7081
Bruno Santana <brunoguimaraessantana@hotmail.com>
2020-01-09 15:18:07 -0300

    Main View Controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,181 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	@FXML
+	private Label labelMovimentacaoCaixa;
+	
+	@FXML
+	private Label labelLancamentos;
+	
+	@FXML
+	private Label labelFluxo;
+	
+	@FXML
+	private Label labelAPagarReceber;
+	
+	@FXML
+	private Label labelPagasRecebidas;
+	
+	@FXML
+	private Label labelExtratoDeContas;
+	
+	@FXML
+	private Label labelCartaoDeCredito;
+	
+	@FXML
+	private Label labelMetas;
+	
+	@FXML
+	private Label labelOrcameto;
+	
+	@FXML
+	private Label labelEconomia;
+	
+	@FXML
+	private Label labelRelatorio;
+	
+	@FXML
+	private Label labelInvestimento;
+	
+	@FXML
+	private Label labelCadastro;
+	
+	@FXML
+	private Label labelCategoria;
+	
+	@FXML
+	private Label labelContas;
+	
+	@FXML
+	private Label labelDocumentos;
+	
+	@FXML
+	private Label labelRegrasDePreenchimento;
+	
+	@FXML
+	private Label labelImportarLancamentos;
+	
+	@FXML
+	private Label labelFechamentoPosicao;
+	
+	@FXML
+	public void onMenuItemAboutClick() {
+		System.out.println("On onMenuItemAboutClick");
+	}
+
+	
+	@FXML
+	public void onLabelMovimentacaoCaixaClick() {
+		System.out.println("On onLabelMovimentacaoCaixaClick");
+	}
+	
+	@FXML
+	public void onLabelLancamentosClick() {
+
+	}
+	
+	@FXML
+	public void onLabelFluxoClick() {
+
+	}
+	
+	@FXML
+	public void onLabelAPagarReceberClick() {
+
+	}
+	
+	@FXML
+	public void onLabelPagasRecebidasClick() {
+
+	}
+	
+	@FXML
+	public void onLabelExtratoDeContasClick() {
+
+	}
+	
+	@FXML
+	public void onLabelCartaoDeCreditoClick() {
+
+	}
+	
+	@FXML
+	public void onLabelMetasClick() {
+
+	}
+	
+	@FXML
+	public void onLabelOrcametoClick() {
+
+	}
+	
+	@FXML
+	public void onLabelEconomiaClick() {
+
+	}
+	
+	@FXML
+	public void onLabelRelatorioClick() {
+
+	}
+	
+	@FXML
+	public void onLabelInvestimentoClick() {
+
+	}
+	
+	@FXML
+	public void onLabelCadastroClick() {
+
+	}
+	
+	@FXML
+	public void onLabelCategoriaClick() {
+
+	}
+	
+	@FXML
+	public void onLabelContasClick() {
+
+	}
+	
+	@FXML
+	public void onLabelDocumentosClick() {
+
+	}
+	
+	@FXML
+	public void onLabelRegrasDePreenchimentoClick() {
+
+	}
+	
+	@FXML
+	public void onLabelImportarLancamentosClick() {
+
+	}
+	
+	@FXML
+	public void onLabelFechamentoPosicaoClick() {
+
+	}
+	
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+
+		
+	}
+
+}
