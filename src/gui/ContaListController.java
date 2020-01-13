90f4bb795aca9ff3bc49330fd9062284b6fd11dd
Bruno Santana <brunoguimaraessantana@hotmail.com>
2020-01-13 13:49:50 -0300

    ContaForm (dialog) Design

--- src/gui/ContaListController.java
+++ src/gui/ContaListController.java
@@ -1,19 +1,28 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Conta;
 import model.services.ContaService;
@@ -63,8 +72,9 @@ public class ContaListController implements Initializable {
 	private ObservableList<Conta> obsList;
 		
 	@FXML
-	public void onBtNewAction () {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction (ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/ContaForm.fxml", parentStage);
 	}
 	
 	public void setContaService (ContaService service) {
@@ -101,8 +111,26 @@ public class ContaListController implements Initializable {
 		List<Conta> list =service.findAll();
 		obsList=FXCollections.observableArrayList(list);
 		tableViewConta.setItems(obsList);
-		
-		
 	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Adicionar Conta");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error Loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
 	
 }
